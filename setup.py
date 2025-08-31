fd83ab03c00b6aa2814330701a71e4cae0d5369f
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2025-09-01 08:42:00 +0900

    changed make install options

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content if "git+" not in x]
-
 setup(
     name="disease-recognition",
     version="0.1.0",
-    install_requires=requirements,
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
